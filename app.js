66f92a7eed2194369b11ac43f1d63356c85b925b
Kyle Wheeless <kyle@wheeless.dev>
2021-03-16 22:01:07 -0700

    Fixed environment issues

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
   console.log(`Server started on ${port}...`);
