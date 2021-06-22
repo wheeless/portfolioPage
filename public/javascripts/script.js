f2220bffc2ee5676efb27fae7f9384bf45ebd4dc
Kyle Wheeless <kyle@wheeless.dev>
2021-06-21 18:39:37 -0700

    nodemodules audit fix

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,5 @@
 console.log("WHAT ARE YOU DOING IN MY SWAMP!?");
 // console.image("http://i.imgur.com/hv6pwkb.png");
-// console.image("https://faceswaponline.com/wp-content/uploads/2017/10/shrek-angry.png");
 
 // console.meme("Updates one route", "Entire project breaks", "Bad Luck Brian", 400, 300);
 // console.meme("Not sure if memes in dev tools is stupid", "or disastrous.", "Not Sure Fry", 125, 100);
@@ -10,10 +9,8 @@ console.log("WHAT ARE YOU DOING IN MY SWAMP!?");
 
 // "Bad Luck Brian"
 
-console.image("/images/oGiMR.gif"); 
+// console.image("/images/oGiMR.gif"); 
 
-console.meme("Not sure if memes in dev tools is stupid", "or disastrous.", "Not Sure Fry");
-console.meme("Not sure if memes in dev tools is stupid", "or disastrous.", "Not Sure Fry", 400, 300);
-console.meme("Not sure if memes in dev tools is stupid", "or disastrous.", "http://i.imgur.com/vu4zTYT.jpg", 400, 300);
+console.meme("YOU", "What are you doing in my swamp", "/images/shrek-angry2.jpg");
 
 console.info("Hosted on my own servers and backed by cloudflare");
