991ce7ca54ab3c83a7b102bc018a1b905b383c47
Kyle Wheeless <kyle@wheeless.dev>
2021-10-29 16:52:36 -0700

    meeting route

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('comingsoon', { title: 'Coming Soon!' });
 });
 /* GET Utilities page. */
-router.get('/utilities', function(req, res, next) {
+router.get('/utilities', function (req, res, next) {
   res.render('utility', { title: 'Our Utilities' });
 });
 
 /* Shrek page */
-router.get('/shrek', function(req, res, next) {
+router.get('/shrek', function (req, res, next) {
   res.render('shrek', { title: 'DONKEY!' });
 });
 
 /* GET Image page. */
-router.get('/images', function(req, res, next) {
+router.get('/images', function (req, res, next) {
   res.redirect('/shrek');
 });
 /* GET Github page. */
-router.get('/github', function(req, res, next) {
+router.get('/github', function (req, res, next) {
   res.redirect('https://github.com/wheeless');
 });
 /* GET Instagram page. */
-router.get('/instagram', function(req, res, next) {
+router.get('/instagram', function (req, res, next) {
   res.redirect('https://www.instagram.com/kwheel_s/');
 });
 /* GET Linkedin page. */
-router.get('/linkedin', function(req, res, next) {
+router.get('/linkedin', function (req, res, next) {
   res.redirect('https://www.linkedin.com/in/kylewheeless/');
+});
+
+/* GET Meeting page. */
+router.get('/meeting', function (req, res, next) {
+  res.redirect('https://calendly.com/kyle-wheeless/');
 });
 
 module.exports = router;
